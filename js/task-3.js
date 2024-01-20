2d621f1c5524e263cacecc3f83fbf2b9512fbe57
Mykhail <mikeps4world@gmail.com>
2024-01-20 18:25:46 +0200

    fix

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,6 @@
 function checkForSpam(message) {
-  if (message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale')) {
-    return true;
-  } else {
-    return false;
-  }
+   return message.toLowerCase().includes('spam') || message.toLowerCase().includes('sale');
+ 
 }
 
 console.log(checkForSpam('Latest technology news')); // false
